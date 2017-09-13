bf48beae4144ea1ca848cd212901801b96bc609d
Adam Ever-Hadani <adamhadani@gmail.com>
2017-09-13 09:45:47 -0700

    WIP

--- sklearn_hierarchical/constants.py
+++ sklearn_hierarchical/constants.py
@@ -3,5 +3,6 @@ Constants.
 
 """
 
-# Special id reserved for a 'root node'
+# Special id reserved for an artificial 'root node' that may be added to class hierarchy
+# when using a 'one classifier per parent node' strategy.
 ROOT = -1
