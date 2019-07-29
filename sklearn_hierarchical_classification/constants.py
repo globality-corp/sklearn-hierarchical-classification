45d4aff99827299533456fc22a338a1a9e5abfba
fbenites <iskywalker@gmx.de>
2019-07-29 18:58:26 +0200

    fixed tests issues, allowing decision_function only on purpose to be used

--- sklearn_hierarchical_classification/constants.py
+++ sklearn_hierarchical_classification/constants.py
@@ -5,8 +5,8 @@ Constants.
 
 # Special id reserved for an artificial 'root node' that may be added to class hierarchy
 # when using a 'one classifier per parent node' strategy.
-#ROOT = "<ROOT>"
-ROOT = -1
+ROOT = "<ROOT>"
+#ROOT = -1
 
 # Dictionary keys used in various places by classifier
 CLASSIFIER = "classifier"
