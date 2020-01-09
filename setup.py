76fe25b3db0e44d5b2dd30a9bd8a2abeeb1c05c4
Adam Ever-Hadani <adamhadani@globality.com>
2020-01-09 07:32:59 -0800

    update setup.py to include long description for display in PyPI

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
+from os import path
+
 from setuptools import find_packages, setup
+
 
 project = "sklearn-hierarchical-classification"
 version = "1.3.0"
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 
 setup(
     name=project,
     version=version,
     description="Hierarchical classification interface extensions for scikit-learn",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Globality Engineering",
     author_email="engineering@globality.com",
     url="https://github.com/globality-corp/sklearn-hierarchical-classification",
